2935e751d1b161d0717452bca83dd1eaad016ecc
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-09-07 17:40:59 -0300

    Update 115. Distinct Subsequences.py

--- Distinct Subsequences/115. Distinct Subsequences.py
+++ Distinct Subsequences/115. Distinct Subsequences.py
@@ -2,8 +2,17 @@ class Solution:
     def numDistinct(self, s: str, t: str) -> int:
         m, n = len(s), len(t)
         
+        # inicializar a matriz
+        pd = [[0] * (n + 1) for _ in range(m + 1)]
+        
         # caso base
         for i in range(m + 1):
-            dp[i][0] = 1  # tem uma maneira de corresponder um t vazio com qualquer s
+            pd[i][0] = 1  # tem uma maneira de corresponder um t vazio com qualquer s
         for j in range(1, n + 1):
-            dp[0][j] = 0  # não tem maneira de corresponder 
+            pd[0][j] = 0  # não tem maneira de corresponder um t não vazio com um s vazio
+        
+        # preencher a matriz 
+        for i in range(1, m + 1):
+            
+        
+        return pd[m][n]
