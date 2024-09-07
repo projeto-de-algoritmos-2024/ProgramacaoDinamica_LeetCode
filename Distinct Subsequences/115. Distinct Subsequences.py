8a47e3965c4b1b1d15578b05587e6b0ca0b9dd39
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-09-07 16:05:05 -0300

    Update 115. Distinct Subsequences.py

--- Distinct Subsequences/115. Distinct Subsequences.py
+++ Distinct Subsequences/115. Distinct Subsequences.py
@@ -1,3 +1,9 @@
 class Solution:
     def numDistinct(self, s: str, t: str) -> int:
+        m, n = len(s), len(t)
         
+        # caso base
+        for i in range(m + 1):
+            dp[i][0] = 1  # tem uma maneira de corresponder um t vazio com qualquer s
+        for j in range(1, n + 1):
+            dp[0][j] = 0  # não tem maneira de corresponder 
