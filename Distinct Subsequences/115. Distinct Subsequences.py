5f36be2ed7058977d54d4ea38077b2fbcaf08304
Harleny <101184511+Angelicahaas@users.noreply.github.com>
2024-09-07 18:08:27 -0300

    Update 115. Distinct Subsequences.py

--- Distinct Subsequences/115. Distinct Subsequences.py
+++ Distinct Subsequences/115. Distinct Subsequences.py
@@ -11,8 +11,12 @@ class Solution:
         for j in range(1, n + 1):
             pd[0][j] = 0  # não tem maneira de corresponder um t não vazio com um s vazio
         
-        # preencher a matriz 
+        # preenche a matriz
         for i in range(1, m + 1):
-            
+            for j in range(1, n + 1):
+                if s[i - 1] != t[j - 1]:
+                    pd[i][j] = pd[i - 1][j - 1]  
+                else:
+                    pd[i][j] = pd[i - 1][j]
         
         return pd[m][n]
